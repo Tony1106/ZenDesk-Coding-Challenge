581638736074586778ee71903724c1f5be3e88b9
tien bui <buiminhtien06@gmail.com>
2019-06-07 10:27:22 +1000

    Add route to get all the tickets with pagination

--- server/modules/Tickets/Controller.js
+++ server/modules/Tickets/Controller.js
@@ -10,6 +10,21 @@ async function handleError(error) {
 }
 module.exports = {
   index: function(req, res) {
+    let { per_page, page } = req.query;
+    let endpoint = "/tickets.json";
+    if (per_page && page) {
+      endpoint = `${endpoint}?per_page=${per_page}&page=${page}`;
+    }
+    tickets
+      .request(endpoint, "get")
+      .then(response => {
+        return res.send(response);
+      })
+      .catch(err => {
+        response.json(res, null, err, null);
+      });
+  },
+  getUser: function(req, res) {
     tickets
       .request("/users.json", "get")
       .then(response => {
