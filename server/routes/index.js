0c662e4582851f2336251ca1cf13fd599ec82963
tien bui <buiminhtien06@gmail.com>
2019-06-10 21:58:50 +1000

    Server: update directory

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,6 +1,8 @@
 module.exports = function(app, db) {
-  //Home page
-  require("../modules/Tickets/Route")(app, db);
+  //_________Register the services below___________
+
+  //Zendesk service
+  require("../modules/Zendesk/Route")(app, db);
   // Handle errors
   require("./errors")(app, db);
 };
